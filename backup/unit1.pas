d0df352b4fcbc5bb37b1e81b3c9a40c852a57a49
SmallPlayz <900raiyan@gmail.com>
2024-04-15 23:22:03 -0400

    Calculating mostly works now.

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Grids,
-  ExtCtrls, TASources;
+  ExtCtrls, TASources, Math, fpexprpars;
 
 type
 
@@ -46,11 +46,34 @@ implementation
 
 function evaluateEquation(expression: string; x: Double): Double;
 var
+  FParser: TFPExpressionParser;
   theResult: Double;
 begin
-  expression := StringReplace(expression, 'x', FloatToStr(x), [rfReplaceAll]);
-  //ShowMessage(expression);
-  // TODO: do the parsing stuff
+
+  // Preset theResult to NaN
+  theResult := NaN;
+  FParser := TFPExpressionParser.Create(nil);
+
+  // Remove "y=" if it appears at the beginning of the expression
+  if Copy(expression, 1, 2) = 'y=' then
+    Delete(expression, 1, 2);
+
+  //expression := StringReplace(expression, 'x', '(' + FloatToStr(x) + ')', [rfReplaceAll]);
+
+  try
+    // Now here go through the expression and calculate it.
+    FParser.BuiltIns := [bcMath];
+    FParser.Identifiers.AddFloatVariable('x', x);
+    FParser.Expression := expression;
+    theResult := FParser.Evaluate.ResFloat;
+  except
+    // Do nothing, simply continue execution.
+  end;
+
+  FParser.Free;
+
+  //ShowMessage(theResult);
+
   evaluateEquation := theResult;
 end;
 
@@ -68,10 +91,33 @@ procedure TmyApp.Edit1Change(Sender: TObject);
 var
   Expression: string;
   x, y: Double;
+  i: Integer;
+const
+  Radius = 3;
 begin
-  Expression := Edit1.Text;
-  y := evaluateEquation(Expression, 4.0);
+  // Clear the drawing on PaintBox1
+  PaintBox1.Canvas.Brush.Color := clWhite;
+  PaintBox1.Canvas.FillRect(PaintBox1.ClientRect);
 
+  Expression := Edit1.Text;
+  y := evaluateEquation(Expression, 4.0); // ADD FOR LOOPS AND STUFFS HERE ALSO
+  for i := 0 to 500 do
+  begin
+    x := i;
+    try
+      y := evaluateEquation(Expression, i);
+      // Convert coordinates to pixels
+      x := x * 1 + PaintBox1.Width div 2; // Scale x to fit the canvas width
+      y := -y * 1 + PaintBox1.Height div 2; // Scale and invert y to fit the canvas height
+
+      // Plot a point
+      Canvas.Brush.Color := clRed;
+      Canvas.Ellipse(Round(x) - Radius, Round(y) - Radius, Round(x) + Radius, Round(y) + Radius);
+    except
+      // do nothing
+    end;
+    //ShowMessage('you got: ' + FloatToStr(y) + ' from i=' + IntToStr(i));
+  end;
 end;
 
 procedure TmyApp.PaintBox1MouseDown(Sender: TObject; Button: TMouseButton;
